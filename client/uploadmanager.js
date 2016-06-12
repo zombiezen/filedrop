38c81ca6242a38eb91b9b1269711eb3d47537541
Aaron Kosel <a_kosel@yahoo.com>
2016-06-11 22:22:35 -0500

    Simplify fetch. Use auto-incrementing id for files.

--- client/uploadmanager.js
+++ client/uploadmanager.js
@@ -39,6 +39,9 @@ filedrop.UploadManager = function($q, Files, $log) {
 
   /** @export {!Object} */
   this.uploads = {};
+
+  /** @private {!Number} */
+  this.id = 0;
 };
 
 /**
@@ -48,7 +51,7 @@ filedrop.UploadManager = function($q, Files, $log) {
  */
 filedrop.UploadManager.prototype.add = function(file) {
   var upload = this.filesService_.upload(file);
-  var id = file.name;
+  var id = this.id++;
   this.uploads[id] = upload.then(angular.bind(this, function(val) {
       this.uploads[id].status = 'success';
       return val;
@@ -105,19 +108,11 @@ filedrop.UploadManager.prototype.makeUploads = function(files) {
 filedrop.UploadManager.prototype.fetch = function(ids) {
   var promises = [];
 
-  if (!ids) {
-    angular.forEach(this.uploads, function(promise, id) {
-      promises.push(promise);
-    }, this);
-  } else {
-    angular.forEach(ids, function(id) {
-      promises.push(this.uploads[id]);
-    }, this);
-  }
+  angular.forEach(ids, function(id) {
+    promises.push(this.uploads[id]);
+  }, this);
 
-  return this.q_.all(promises).then(angular.bind(this, function(val) {
-    return arguments[0];
-  }));
+  return this.q_.all(promises);
 };
 
 /**
@@ -125,5 +120,11 @@ filedrop.UploadManager.prototype.fetch = function(ids) {
  * @return {!angular.$q.Promise}
  */
 filedrop.UploadManager.prototype.fetchAll = function() {
-  return this.fetch();
+  var ids = [];
+
+  angular.forEach(this.uploads, function(promise, id) {
+    ids.push(id);
+  }, this);
+
+  return this.fetch(ids);
 };
